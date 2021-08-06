48040de72fb9e5bb8a0d41661a4ed877fcbca6dc
apothuru <apothuru@vmware.com>
2021-08-05 18:09:42 -0700

    fixed deployment title length

--- vrce-ui/src/components/card/card.js
+++ vrce-ui/src/components/card/card.js
@@ -7,7 +7,7 @@ import { CardContainer } from "./card.styles";
 const Card = (props) => {
     return(
         <CardContainer onClick={() => props.history.push(`/detail/${props.deployment.id}`)}>
-            <Typography variant="h5" component="h2">{props.deployment.title}</Typography>
+            <Typography variant="h5" component="h2">Deployment Name: {props.deployment.title.substring(0, 21)}</Typography>
             <Typography color="yellow">User ID: {props.deployment.userId}</Typography>
             <Typography >Data Center Type: { props.deployment.id % 3 === 0 ? "Private" : "Public" }</Typography>
         </CardContainer>
